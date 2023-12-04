75aa2d7de25bcf72e27ec39e0b655dc6e5a56b33
vipinWednesday <vipin@wednesday.is>
2023-12-04 18:41:18 +0530

    feat: added gen-ui

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,118 +1,205 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { Inter } from "next/font/google";
+import { useState } from "react";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
+const Integration = () => {
+  const [file, setFile] = useState(null);
+  const [email, setEmail] = useState("");
+  const [categories, setCategories] = useState({
+    product: [],
+    coaching: [],
+    community: [],
+    ebook: [],
+    external_product: [],
+  });
+
+  const [loading, setLoading] = useState(false);
+  const [emailError, setEmailError] = useState("");
+
+  const handleFileChange = (event) => {
+    setFile(event.target.files[0]);
+  };
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+    setEmailError(""); // Reset email error on change
+  };
+
+  const handleKeywordChange = (category, keywords) => {
+    setCategories({ ...categories, [category]: keywords });
+  };
+
+  const handleBackspace = (category) => {
+    if (!categories[category].length) {
+      // Remove the category if there are no keywords
+      const updatedCategories = { ...categories };
+      delete updatedCategories[category];
+      setCategories(updatedCategories);
+    }
+  };
+
+  /*  product_signals = ["store", "sale", "storefront", "purchase", "buy", "product", "cart", "discount", "price", "credit card", "paypal", "rewards"]
+coaching_signals = ["mentor", "mentoring", "guru", "gurus", "1 on 1", "1:1", "class", "classes", "consultant", "business", "businesses", "masterclass"]
+ebooks_signals = ["ebook", "ebooks", "pdf", "download", "book", "guide", "kindle", "mobi", "epub", "author", "authors", "books", "published", "reader"]
+external_store_signals = ["amazon", "craigslist", "ebay", "bestbuy", "wish", "walmart"] */
+
+  const validateEmail = () => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+      setEmailError("Invalid email address");
+      return false;
+    }
+    return true;
+  };
+
+  const constructCustomSignals = () => {
+    const customSignals = [];
+
+    for (const category in categories) {
+      if (categories[category].length > 0) {
+        customSignals.push({
+          signal: categories[category],
+          category: category,
+        });
+      }
+    }
+
+    return JSON.stringify(customSignals);
+  };
+
+  const handleSubmit = () => {
+    // Validate email before submission
+    if (!validateEmail()) {
+      return;
+    }
+
+    setLoading(true);
+
+    const formData = new FormData();
+    formData.append("file", file);
+    formData.append("email", email);
+    formData.append("custom_signals", constructCustomSignals());
+
+    fetch("http://34.123.57.61:6000/getLeads", {
+      method: "POST",
+      body: formData,
+    })
+      .then((response) => response.text())
+      .then((data) => {
+        console.log(data);
+        setLoading(false);
+        alert("You will receive an email shortly");
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+        setLoading(false);
+      });
+  };
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <div className="container mx-auto my-10 w-[100vw] lg:w-[40vw] px-4">
+      <h1 className="text-3xl font-bold mb-4">Lead keywords Report</h1>
+      <p className="text-sm text-gray-400">
+        Upload the csv and input your Keywords
+      </p>
+      <input type="file" className="mt-4" onChange={handleFileChange} />
+      <div className="my-4">
+        <label className="block mb-2">Email:</label>
+        <input
+          type="text"
+          placeholder="Enter your email"
+          value={email}
+          onChange={handleEmailChange}
+          className={`block w-full p-2 border rounded text-gray-800 ${
+            emailError ? "border-red-500" : ""
+          }`}
+        />
+        {emailError && <p className="text-red-500 text-sm">{emailError}</p>}
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+      <div className="my-4">
+        <h2 className="text-xl font-semibold mb-2">Keywords</h2>
+        <label className="block mb-2">Product:</label>
+        <input
+          type="text"
+          placeholder="Enter keywords (comma-separated)"
+          value={categories.product.join(",")}
+          onChange={(e) =>
+            handleKeywordChange("product", e.target.value.split(","))
+          }
+          onKeyDown={(e) => e.key === "Backspace" && handleBackspace("product")}
+          className="block w-full p-2 border rounded text-gray-800"
         />
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="my-4">
+        <label className="block mb-2">Coaching:</label>
+        <input
+          type="text"
+          placeholder="Enter keywords (comma-separated)"
+          value={categories.coaching.join(",")}
+          onChange={(e) =>
+            handleKeywordChange("coaching", e.target.value.split(","))
+          }
+          onKeyDown={(e) =>
+            e.key === "Backspace" && handleBackspace("coaching")
+          }
+          className="block w-full p-2 border rounded text-gray-800"
+        />
       </div>
-    </main>
-  )
-}
+      <div className="my-4">
+        <label className="block mb-2">Community:</label>
+        <input
+          type="text"
+          placeholder="Enter keywords (comma-separated)"
+          value={categories.community.join(",")}
+          onChange={(e) =>
+            handleKeywordChange("community", e.target.value.split(","))
+          }
+          onKeyDown={(e) =>
+            e.key === "Backspace" && handleBackspace("community")
+          }
+          className="block w-full p-2 border rounded text-gray-800"
+        />
+      </div>
+      <div className="my-4">
+        <label className="block mb-2">Ebook:</label>
+        <input
+          type="text"
+          placeholder="Enter keywords (comma-separated)"
+          value={categories.ebook.join(",")}
+          onChange={(e) =>
+            handleKeywordChange("ebook", e.target.value.split(","))
+          }
+          onKeyDown={(e) => e.key === "Backspace" && handleBackspace("ebook")}
+          className="block w-full p-2 border rounded text-gray-800"
+        />
+      </div>
+      <div className="my-4">
+        <label className="block mb-2">External Product:</label>
+        <input
+          type="text"
+          placeholder="Enter keywords (comma-separated)"
+          value={categories.external_product.join(",")}
+          onChange={(e) =>
+            handleKeywordChange("external_product", e.target.value.split(","))
+          }
+          onKeyDown={(e) =>
+            e.key === "Backspace" && handleBackspace("external_product")
+          }
+          className="block w-full p-2 border rounded text-gray-800"
+        />
+      </div>
+      <button
+        onClick={handleSubmit}
+        className={`${
+          loading ? "bg-gray-400 cursor-not-allowed" : "bg-blue-500"
+        } text-white px-4 py-2 rounded disabled:cursor-not-allowed`}
+        disabled={!file || !email || loading || emailError}
+      >
+        {loading ? "Loading..." : "Submit"}
+      </button>
+    </div>
+  );
+};
+
+export default Integration;
